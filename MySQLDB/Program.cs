ea22b2e99c69b9c9a5765d43149a26e7815e8b12
Serhiy Kulchytsky <serhiyx@mail.ru>
2015-03-17 19:42:05 +0200

    Uploading

---
+++ MySQLDB/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace MySQLDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MySqlConnection myConnection = new MySqlConnection("Server=localhost; Database=database; Uid=Sergio; Pwd=123456");
+
+            //OPEN CONNECTION
+            myConnection.Open();
+
+            //CREATE TABLE STATEMENT
+            MySqlCommand myCommand = new MySqlCommand();
+            myCommand.Connection = myConnection;
+            myCommand.CommandText = " CREATE TABLE Persons " +
+                                    " ( " +
+                                        " ID INT(5) NOT NULL AUTO_INCREMENT, " +
+                                        " Name VARCHAR(255) NOT NULL, " +
+                                        " City VARCHAR(255), " +
+                                        " PRIMARY KEY (ID)" +
+                                    " ) ";
+
+            myCommand.ExecuteNonQuery();
+
+            //-------------------------------------------------------
+            //INSERT STATEMENT
+            myCommand = new MySqlCommand();
+            myCommand.Connection = myConnection;
+            myCommand.Parameters.Add("@Name", MySqlDbType.VarChar).Value = "SERGIO";
+            myCommand.Parameters.Add("@City", MySqlDbType.VarChar).Value = "Ternopil";
+
+            myCommand.CommandText = "INSERT INTO Persons (Name, City) VALUES (@Name, @City);";
+            myCommand.ExecuteNonQuery();
+
+            //------------------------------------------------------
+            //UPDATE STATEMENT
+            myCommand = new MySqlCommand();
+            myCommand.Connection = myConnection;
+            myCommand.Parameters.Add("@Name", MySqlDbType.VarChar).Value = "SERGIO";
+            myCommand.Parameters.Add("@NewName", MySqlDbType.VarChar).Value = "SEHIY";
+            myCommand.CommandText = "UPDATE Persons SET Name = @NewName WHERE Name =  @Name;";
+            myCommand.ExecuteNonQuery();
+
+            //------------------------------------------------------
+            //SELECT STATEMENT
+            myCommand = new MySqlCommand();
+            myCommand.Connection = myConnection;
+            myCommand.Parameters.Add("@City", MySqlDbType.VarChar).Value = "Ternopil";
+            myCommand.CommandText = "SELECT Name FROM Persons WHERE City = @City;";
+            MySqlDataReader reader = myCommand.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine(reader["Name"]);
+            }
+            reader.Close();
+
+            //-----------------------------------------------------
+            //DELETE STATEMENT
+            myCommand = new MySqlCommand();
+            myCommand.Connection = myConnection;
+            myCommand.Parameters.Add("@City", MySqlDbType.VarChar).Value = "Ternopil";
+            myCommand.CommandText = "DELETE FROM Persons WHERE City = @City;";
+            myCommand.ExecuteNonQuery();
+
+            myCommand.CommandText = "DROP TABLE Persons;";
+            myCommand.ExecuteNonQuery();
+
+            Console.ReadLine();
+
+            //CLOSE CONNECTION
+            myConnection.Close();
+        }
+    }
+}
